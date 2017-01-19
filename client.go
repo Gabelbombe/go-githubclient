53e0e6686b0bc2576b70637d72c9202d805686f3
ehime <dodomeki@gmail.com>
2017-01-19 06:06:40 -0800

    adding handlers login and main

--- client.go
+++ client.go
@@ -8,8 +8,35 @@ import (
 	githuboauth "golang.org/x/oauth2/github"
 )
 
+var (
+	// You must register the app at https://github.com/settings/applications
+	// Set callback to http://127.0.0.1:7000/github_oauth_cb
+	// Set ClientId and ClientSecret to
+	oauthConf = &oauth2.Config{
+		ClientID:     "",
+		ClientSecret: "",
+		Scopes:       []string{"user:email", "repo"},
+		Endpoint:     githuboauth.Endpoint,
+	}
+	// random string for oauth2 API calls to protect against CSRF
+	oauthStateString = "thisshouldberandom"
+)
 
 const htmlIndex = `<html><body>
 Logged in with <a href="/login">GitHub</a>
 </body></html>
 `
+
+
+// main handler
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(htmlIndex))
+}
+
+
+// login handler
+func handleGitHubLogin(w http.ResponseWriter, r *http.Request) {
+	url := oauthConf.AuthCodeURL(oauthStateString, oauth2.AccessTypeOnline)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
